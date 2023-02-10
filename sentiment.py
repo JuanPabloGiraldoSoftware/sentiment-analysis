45b340cd174a3c97e61d3a91b602d08f105534ea
JuanPabloGiraldoSoftware <juanpgym@gmail.com>
2023-02-10 14:02:31 -0500

    added analysis.txt

--- sentiment.py
+++ sentiment.py
@@ -161,14 +161,14 @@ def training_model():
     print(X)
     print(Y)
     print("###################################################")
-    vectorizer = TfidfVectorizer() #mejor desempeño sobre el CountVectorizer a cambio de costo computacional
+    vectorizer = TfidfVectorizer() #Better performance than CountVectorizer in exchange of computational cost
     x_train, x_test, y_train, y_test = train_test_split(X,Y,stratify=Y, test_size=0.30)
     print_shape(x_train,x_test)
     x_train_bow=vectorizer.fit_transform(x_train)
     x_test_bow=vectorizer.transform(x_test)
     print_shape(x_train_bow,x_test_bow)
 
-    #KNearestNeighbors X -> Costo computacional alto
+    #KNearestNeighbors X -> large execution time
     #knn=KNeighborsClassifier()
     #k_range=list(range(1,50))
     #options=['uniform', 'distance']
@@ -215,7 +215,7 @@ def training_model():
     print_metrics(y_test,y_pred_dtc)
     print("============================================")   
 
-    #Support Vector Macine  X -> Costo computacional alto
+    #Support Vector Macine  X -> large execution time
     #svc=SVC(C = 100, kernel = 'linear', random_state=123)
     #svc.fit(x_train_bow,y_train)
     #y_pred_svc = svc.predict(x_test_bow)
@@ -231,7 +231,7 @@ def training_model():
     print_metrics(y_test,y_pred_gnbc)
     print("============================================")   
 
-def testing_k_neighbors():
+def testing_k_neighbors(x_train_bow,y_train,x_test_bow,y_test):
     accuracy_hist = []
     for i in range (1,21):
         knn=KNeighborsClassifier(n_neighbors=i)
